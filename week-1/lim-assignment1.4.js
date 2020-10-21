7ab4eb15e5617b77494d3439e677793c23e19a59
Eunice Lim <eunicelim31@gmail.com>
2020-10-21 11:33:15 -0500

    Adding assignment 1.4.js

---
+++ week-1/lim-assignment1.4.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Eunice Lim
+; Date: 20 Oct 2020
+; Modified By: Eunice Lim
+; Description: Assignment 1.4
+;===========================================
+*/ 
+
+var firstName = "Eunice";
+var lastName = "Lim";
+var age = 32;
+var street = "100 Bamboo Ave";
+var city = "Tokyo";
+var zipCode = "98765";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
