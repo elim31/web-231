6ad4185e32366b320108e1b59d68c2ebe3e9db1e
Eunice Lim <eunicelim31@gmail.com>
2020-10-21 11:29:55 -0500

    Adding Hello World

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Eunice Lim
+; Date: 20 Oct 2020
+; Modified By: Eunice Lim
+; Description: Exercise 1.3 Hello World
+;===========================================
+*/
+console.log("Hello World, my name is Eunice Lim");
