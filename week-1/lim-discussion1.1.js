bc8e73a2662962bc30074fc772ed2d819ef1e21f
Eunice Lim <eunicelim31@gmail.com>
2020-10-25 21:59:49 -0500

    Adding discussion 1.1

---
+++ week-1/lim-discussion1.1.js
@@ -0,0 +1,20 @@
+ /*
+============================================
+; Title: Discussion 1.1
+; Author: Eunice Lim 
+; Date: 19 October 2020
+; Modified By: Eunice Lim
+; Description: A simple JavaScript program using values
+; and variables with at least two errors.
+;===========================================
+*/
+<script>
+function myDog() {   
+var name 
+=
+Hank, the dog
++ ‘a Great Pyrenees’
+var n = name.toString(); 
+console.log(n);       //wanting this to print as a sentence
+}
+</script>
