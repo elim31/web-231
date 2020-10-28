00eb7e42d99db7b49221792519dfd0a48aa09a6d
Eunice Lim <eunicelim31@gmail.com>
2020-10-28 11:43:15 -0500

    Adding Exercise 2.2

---
+++ week-2/lim-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   27 Oct 2020
+; Modified By: Eunice Lim
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../lim-header.js');
+
+console.log(header.display("Eunice", "Lim", "Exercise 2.2"));
